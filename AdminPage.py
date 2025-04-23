0e85a0a46160cad2415acb55f919f837de1f0014
David Selvaraj <166501046+david03dev@users.noreply.github.com>
2025-04-23 22:43:37 +0530

    Update AdminPage.py

--- AdminPage.py
+++ AdminPage.py
@@ -15,13 +15,13 @@ class AdminPage:
         self.Configuration = (By.XPATH, "//span[contains(text(), 'Configuration')]")
         
         #Menu page
-        self.Admin = (By.XPATH, "//span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']") #//span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']
-        self.PIM = (By.XPATH, "//a[@class='oxd-main-menu-item']/span[text()='PIM']") #//a[@class='oxd-main-menu-item']/span[text()='PIM']
+        self.Admin = (By.XPATH, "//span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']")
+        self.PIM = (By.XPATH, "//a[@class='oxd-main-menu-item']/span[text()='PIM']")
         self.Leave = (By.XPATH, "//span[text()='Leave']")#//span[text()='Leave']
-        self.Time = (By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Time']") #//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Time']
-        self.Recruitment = (By.XPATH, "//span[text()='Recruitment']")#//span[text()='Recruitment']
-        self.my_info = (By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='My Info']")#//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='My Info']
-        self.Performance = (By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Performance']")#//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Performance']
+        self.Time = (By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Time']") 
+        self.Recruitment = (By.XPATH, "//span[text()='Recruitment']")
+        self.my_info = (By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='My Info']") 
+        self.Performance = (By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Performance']")
 
         
         

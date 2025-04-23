65ae2d4f821ec8827a816054a489731e5a3ec0cd
David Selvaraj <166501046+david03dev@users.noreply.github.com>
2025-04-23 22:32:58 +0530

    Update AdminPage.py

--- AdminPage.py
+++ AdminPage.py
@@ -6,13 +6,13 @@ class AdminPage:
     def __init__(self, driver):
         self.driver = driver
         self.page_title = "OrangeHRM"
-        self.user_management = (By.XPATH, "//span[contains(text(), 'User Management')]") #//span[contains(text(), 'User Management')]
-        self.job = (By.XPATH, "//li[span[contains(@class, 'oxd-topbar-body-nav-tab-item') and contains(text(), 'Job')]]") #//li[span[contains(@class, 'oxd-topbar-body-nav-tab-item') and contains(text(), 'Job')]]
-        self.organization = (By.XPATH, "//span[contains(text(), 'Organization')]")#//span[contains(text(), 'Organization')]
-        self.Qualifications = (By.XPATH, "//span[contains(text(), 'Qualifications')]") #//span[contains(text(), 'Qualifications')]
-        self.Nationalities = (By.XPATH, "//a[contains(text(), 'Nationalities')]")#//a[contains(text(), 'Nationalities')]
-        self.Corporate = (By.XPATH, "//a[contains(text(), 'Corporate Branding')]")#//a[contains(text(), 'Corporate Branding')]
-        self.Configuration = (By.XPATH, "//span[contains(text(), 'Configuration')]")#//span[contains(text(), 'Configuration')]
+        self.user_management = (By.XPATH, "//span[contains(text(), 'User Management')]")
+        self.job = (By.XPATH, "//li[span[contains(@class, 'oxd-topbar-body-nav-tab-item') and contains(text(), 'Job')]]") 
+        self.organization = (By.XPATH, "//span[contains(text(), 'Organization')]")
+        self.Qualifications = (By.XPATH, "//span[contains(text(), 'Qualifications')]") 
+        self.Nationalities = (By.XPATH, "//a[contains(text(), 'Nationalities')]")
+        self.Corporate = (By.XPATH, "//a[contains(text(), 'Corporate Branding')]")
+        self.Configuration = (By.XPATH, "//span[contains(text(), 'Configuration')]")
         
         #Menu page
         self.Admin = (By.XPATH, "//span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']") #//span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']

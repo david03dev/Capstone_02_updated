5ed1cae244b3c59725508c66290464601313f921
David Selvaraj <dsraj03@gmail.com>
2025-09-11 20:25:30 +0530

    conftest setup completed

--- conftest.py
+++ conftest.py
@@ -1,1 +1,15 @@
+import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver_setup():
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
+    yield driver
+    driver.quit()
+
+

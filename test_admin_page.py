ec08c80d1952a7e4a374ee4fcaf890355e55ea46
David Selvaraj <dsraj03@gmail.com>
2025-09-29 23:28:13 +0530

    adding second test case

--- test_admin_page.py
+++ test_admin_page.py
@@ -45,9 +45,17 @@ class TestAdminPage:
                 EC.visibility_of_element_located(option)
             )
 
-        def test_options_validation(self, driver_setup)
+        def test_options_validation(self, driver_setup):
             driver = driver_setup
-        # Step 3: Validate All menu in admin page
+            # Login as Admin
+            login_page = LoginPage(driver)
+            login_page.enter_username("Admin")
+            login_page.enter_password("admin123")
+            login_page.click_login()
+
+            # open admin page
+            login_page.click_admin_page()
+            # Step 3: Validate All menu in admin page
             all_menu = [
                 admin_page.PIM, admin_page.Leave, admin_page.Time, admin_page.Recruitment,admin_page.my_info, admin_page.Performance,
                 admin_page.Dashboard, admin_page.Directory, admin_page.Maintenance, admin_page.Buzz
